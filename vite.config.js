4a42d8284c855ec733c222aabc65fa34894be95b
Yardan <139103935+yardanshaq@users.noreply.github.com>
2025-08-18 01:01:45 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/PortofolioV2/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
