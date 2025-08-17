7ae00e5402a9aa87afa8f5bd2204c5b54bacec39
Yardan <139103935+yardanshaq@users.noreply.github.com>
2025-08-18 03:07:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -92,19 +92,9 @@ function App() {
               className=" mb-6"
             />
             <div className="flex items-center sm:gap-4 gap-2">
-              <button
-                onClick={() => {
-                  const link = document.createElement('a');
-                  link.href = '/cv/CV Yardan.pdf';
-                  link.download = 'CV Yardan.pdf';
-                  document.body.appendChild(link);
-                  link.click();
-                  document.body.removeChild(link);
-                }}
-                className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
-              >
+              <a href="/cv/CV.pdf" download="Yardan_CV.pdf" className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors">
                 <ShinyText text="Download CV" disabled={false} speed={3} className="custom-class" />
-              </button>
+              </a>
               
               <a href="#project" className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors">
                 <ShinyText text="Explore My Projects" disabled={false} speed={3} className="custom-class" />
